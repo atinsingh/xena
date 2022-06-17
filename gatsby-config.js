8e89993826a9bbcad5c2974958abaa64813d45eb
Atin Singh <atin@pragra.io>
2022-06-17 10:59:49 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: `XENA LMS`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "7273727827",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-postcss", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "2839282"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
